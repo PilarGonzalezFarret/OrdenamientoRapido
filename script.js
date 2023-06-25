bffdfcc46b5033649b7291641eb6d0713c8e599a
PilarGonzalezFarret <37448643+PilarGonzalezFarret@users.noreply.github.com>
2023-06-25 19:48:48 -0400

    Desarrollo
    Desarrollo parcial

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//hola haz tu tarea
+let arr = [3,12,4,13,5,7,1,10,2,11,14,6,15,16,8,17,9];
+
+const quickSort = (arr) => {
+    //mi función acá
+}
+
+console.log(quickSort([arr]));
+
+//https://www.youtube.com/watch?v=DYmTpUfcyT8
+
