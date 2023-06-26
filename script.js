fff87f22cd507ad7239bc56ac97a6fefb17eab1d
PilarGonzalezFarret <37448643+PilarGonzalezFarret@users.noreply.github.com>
2023-06-26 14:29:56 -0400

    Desarrollo

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let arr = [3,12,4,13,5,7,1,10,2,11,14,6,15,16,8,17,9];
+//let arr = [3,12,4,13,5,7,1,10,2,11,14,6,15,16,8,17,9];
 
 const quickSort = (arr) => {
-    //mi función acá
+    if (arr.lenght < 1) {
+        return[];
+    }
+    let left = [];
+    let right = [];
+    let pivot = [0];
+
+    for (let i = 1; i < arr.lenght; i++) {
+        if (arr[i]<pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [].concat(quickSort(left), pivot, quickSort(right));
 }
 
-console.log(quickSort([arr]));
+console.log(quickSort([3,12,4,13,5,7,1,10,2,11,14,6,15,16,8,17,9]));
+console.log("hello");
 
-//https://www.youtube.com/watch?v=DYmTpUfcyT8
 
